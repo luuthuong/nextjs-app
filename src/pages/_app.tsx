48821353bd2b83659f79b9b8c0be4a1865935347
luuthuong <luuthuong20022000@gmail.com>
2023-04-09 16:51:06 +0700

    update structure project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ThemeContext } from "@/contexts";
+import { AppProvider } from "@/providers/app.provider";
+import "@/styles/global.css";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeContext.Provider value="dark">
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </ThemeContext.Provider>
+  );
 }
